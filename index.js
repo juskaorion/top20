24a072c5c2cef65347b935c1786cf940d141e7ec
juskaorion <juska.wendland@gmail.com>
2026-05-21 21:50:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const SCORE_CONFIG = {
         COMMENT_VALUE: 12.5,
         WEB_THUMB_VALUE: 5.5,
         WEB_PLAY_VALUE: 10.5,
-        AGE_PENALTY_PER_DAY: 14.5
+        AGE_PENALTY_PER_DAY: 19.5
     },
     
     // Spotifysta tulevien biisien kertoimet
     SPOTIFY: {
         WEB_THUMB_VALUE: 11.0,
         WEB_PLAY_VALUE: 10.5,
-        AGE_PENALTY_PER_DAY: 14.75
+        AGE_PENALTY_PER_DAY: 19.5
     }
 };
 /* ======================================================= */
